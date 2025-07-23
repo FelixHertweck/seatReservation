822149f492da37e68b06d9c12f286b9defed40a2
FelixHertweck <github@felix-hertweck.de>
2025-07-23 23:13:10 +0200

    Add pre-commit script to enforce checks for backend and frontend

---
+++ check-preCommit.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# This script checks if the pre-commit hook is installed and runs it if it is.
+
+# Backend check
+cd "$(dirname "$0")"
+mvn spotless:check || {
+  echo "Spotless check failed. Please fix the issues before committing."
+  exit 1
+}
+mvn test || {
+  echo "Tests failed. Please fix the issues before committing."
+  exit 1
+}
+mvn quarkus:build || {
+  echo "Build failed. Please fix the issues before committing."
+  exit 1
+}
+
+# Frontend check
+cd "$(dirname "$0")"
+cd ./webapp
+npm run lint:check || {
+  echo "Linting failed. Please fix the issues before committing."
+  exit 1
+}
+npm run format:check || {
+  echo "Formatting check failed. Please fix the issues before committing."
+  exit 1
+}
+# api client check
+npm run generate:api
+git diff --exit-code -- api/ || {
+  echo "API client generation failed. Please fix the issues before committing."
+  exit 1
+}
+npm run build || {
+  echo "Build failed. Please fix the issues before committing."
+  exit 1
+}
