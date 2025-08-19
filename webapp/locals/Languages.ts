ee17c8221ceb3b2fcf412fdf960278efdde60c51
FelixHertweck <github@felix-hertweck.de>
2025-08-19 19:18:39 +0200

    Add language support

---
+++ webapp/locals/Languages.ts
@@ -0,0 +1,35 @@
+"use client";
+// Constants for language support
+
+import i18n from "i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import { initReactI18next } from "react-i18next";
+import translationEN from "./en/translation.json";
+import translationDE from "./de/translation.json";
+
+export const LanguageLabels = new Map([
+  ["en", "English"],
+  ["de", "Deutsch"],
+]);
+
+const resources = {
+  en: {
+    translation: translationEN,
+  },
+  de: {
+    translation: translationDE,
+  },
+};
+
+export function initI18N() {
+  void i18n
+    .use(LanguageDetector)
+    .use(initReactI18next)
+    .init({
+      resources,
+      fallbackLng: "en",
+      interpolation: {
+        escapeValue: false,
+      },
+    });
+}
