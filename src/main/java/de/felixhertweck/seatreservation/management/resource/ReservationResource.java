ef5e28f98edc1b9fd6befab6f761d90e38dbd383
FelixHertweck <github@felix-hertweck.de>
2025-11-16 11:48:51 +0100

    refactor: rename management package 'ressource' to 'resource' (fix typo)

--- src/main/java/de/felixhertweck/seatreservation/management/resource/ReservationResource.java
+++ src/main/java/de/felixhertweck/seatreservation/management/resource/ReservationResource.java
@@ -17,7 +17,7 @@
  * limitations under the License.
  * #L%
  */
-package de.felixhertweck.seatreservation.management.ressource;
+package de.felixhertweck.seatreservation.management.resource;
 
 import java.util.Collections;
 import java.util.List;
@@ -25,9 +25,16 @@ import java.util.Set;
 import jakarta.annotation.security.RolesAllowed;
 import jakarta.inject.Inject;
 import jakarta.validation.Valid;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
-import jakarta.ws.rs.*;
 
 import de.felixhertweck.seatreservation.common.exception.EventNotFoundException;
 import de.felixhertweck.seatreservation.management.dto.BlockSeatsRequestDTO;
