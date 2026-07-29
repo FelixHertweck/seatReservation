a923c2ab314057707c8a0577040a2c531c78d793
FelixHertweck <github@felix-hertweck.de>
2026-07-28 23:31:15 -0400

    test: stub seat batch-load in QR code error path test
    sendEventReminder_QrCodeGenerationThrowsException (added on main by #454
    after this branch diverged) didn't stub the new
    seatRepository.findByIdsWithAreaAndEntrance() batch lookup introduced
    here, leaving the seat map empty and NPEing on seat.getSeatNumber().

--- src/test/java/de/felixhertweck/seatreservation/email/EmailServiceTest.java
+++ src/test/java/de/felixhertweck/seatreservation/email/EmailServiceTest.java
@@ -330,6 +330,7 @@ class EmailServiceTest {
         List<Reservation> reservations =
                 Collections.singletonList(createTestReservation(user, event, seat));
 
+        when(seatRepository.findByIdsWithAreaAndEntrance(any())).thenReturn(List.of(seat));
         when(emailSeatMapService.createEmailSeatMapToken(any(), any(), any()))
                 .thenReturn("test-token-123");
         when(emailSeatMapService.getPngImage(anyString())).thenReturn(Optional.of(new byte[0]));
