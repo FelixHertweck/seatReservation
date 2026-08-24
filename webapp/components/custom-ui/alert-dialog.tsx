23c7c1e3dd81cef01e59f182df0bb388fbd9e2bd
Felix Hertweck <52834554+FelixHertweck@users.noreply.github.com>
2026-08-23 23:29:21 -0400

    fix(frontend): increase vertical spacing between text and action buttons in alert dialogs (#584)

--- webapp/components/custom-ui/alert-dialog.tsx
+++ webapp/components/custom-ui/alert-dialog.tsx
@@ -5,7 +5,10 @@
 // See `dialog.tsx` for the rationale.
 
 import * as React from "react";
-import { AlertDialogContent as BaseAlertDialogContent } from "@/components/ui/alert-dialog";
+import {
+  AlertDialogContent as BaseAlertDialogContent,
+  AlertDialogFooter as BaseAlertDialogFooter,
+} from "@/components/ui/alert-dialog";
 import { cn } from "@/lib/utils";
 
 type AlertDialogContentProps = React.ComponentPropsWithoutRef<
@@ -19,8 +22,8 @@ const AlertDialogContent = React.forwardRef<
   <BaseAlertDialogContent
     ref={ref}
     className={cn(
-      "inset-0 top-0 left-0 flex h-full w-full max-w-none translate-x-0 translate-y-0 flex-col justify-center overflow-y-auto rounded-none border-0 p-4",
-      "sm:inset-auto sm:top-[50%] sm:left-[50%] sm:h-auto sm:max-h-[85vh] sm:w-full sm:max-w-xl sm:-translate-x-1/2 sm:-translate-y-1/2 sm:block sm:overflow-visible sm:rounded-lg sm:border sm:p-6",
+      "inset-0 top-0 left-0 flex h-full w-full max-w-none translate-x-0 translate-y-0 flex-col justify-center gap-6 overflow-y-auto rounded-none border-0 p-4",
+      "sm:inset-auto sm:top-[50%] sm:left-[50%] sm:grid sm:gap-6 sm:h-auto sm:max-h-[85vh] sm:w-full sm:max-w-xl sm:-translate-x-1/2 sm:-translate-y-1/2 sm:overflow-visible sm:rounded-lg sm:border sm:p-6",
       className,
     )}
     {...props}
@@ -28,16 +31,30 @@ const AlertDialogContent = React.forwardRef<
 ));
 AlertDialogContent.displayName = "ResponsiveAlertDialogContent";
 
+function AlertDialogFooter({
+  className,
+  ...props
+}: Readonly<React.HTMLAttributes<HTMLDivElement>>) {
+  return (
+    <BaseAlertDialogFooter
+      className={cn(
+        "flex-col-reverse gap-3 sm:flex-row sm:justify-end sm:gap-2",
+        className,
+      )}
+      {...props}
+    />
+  );
+}
+
 export {
   AlertDialog,
   AlertDialogPortal,
   AlertDialogOverlay,
   AlertDialogTrigger,
   AlertDialogHeader,
-  AlertDialogFooter,
   AlertDialogTitle,
   AlertDialogDescription,
   AlertDialogAction,
   AlertDialogCancel,
 } from "@/components/ui/alert-dialog";
-export { AlertDialogContent };
+export { AlertDialogContent, AlertDialogFooter };
